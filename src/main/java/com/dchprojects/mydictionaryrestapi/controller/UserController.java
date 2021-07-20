ced6441d9070010e61c5bd02bdf06ca618bf3a21
Dima <dima.chumakovwork@gmail.com>
2021-07-20 17:17:01 +0300

    added create user method

--- src/main/java/com/dchprojects/mydictionaryrestapi/controller/UserController.java
+++ src/main/java/com/dchprojects/mydictionaryrestapi/controller/UserController.java
@@ -5,10 +5,7 @@ import com.dchprojects.mydictionaryrestapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -33,6 +30,11 @@ public class UserController {
         } catch (NoSuchElementException e) {
             return new ResponseEntity<User>(HttpStatus.NOT_FOUND);
         }
+    }
+
+    @PostMapping
+    public void create(@RequestBody User user) {
+        userService.save(user);
     }
 
 }
