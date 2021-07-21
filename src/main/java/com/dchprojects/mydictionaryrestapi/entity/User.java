2195a209b1c3f84e5db83fe5325cdf1054122086
Dima <dima.chumakovwork@gmail.com>
2021-07-21 18:36:41 +0300

    added: create_at, update_at properties

--- src/main/java/com/dchprojects/mydictionaryrestapi/entity/User.java
+++ src/main/java/com/dchprojects/mydictionaryrestapi/entity/User.java
@@ -1,5 +1,9 @@
 package com.dchprojects.mydictionaryrestapi.entity;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,14 +16,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String username;
+    @CreationTimestamp
+    private Timestamp created_at;
+    @UpdateTimestamp
+    private Timestamp updated_at;
 
     public User() {
 
     }
 
-    public User(Integer id, String username) {
+    public User(Integer id, String username, Timestamp created_at, Timestamp updated_at) {
         this.id = id;
         this.username = username;
+        this.created_at = created_at;
+        this.updated_at = updated_at;
     }
 
     public Integer getId() {
@@ -36,6 +46,22 @@ public class User {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
     }
 
 }
