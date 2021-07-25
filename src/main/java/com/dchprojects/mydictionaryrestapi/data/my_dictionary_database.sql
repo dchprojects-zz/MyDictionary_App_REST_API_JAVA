58c4a33b55ad544cbde6f168aba6fb7cc8986d8d
Dima <dima.chumakovwork@gmail.com>
2021-07-25 18:13:05 +0300

    edited my_dictionary_database.sql file

--- src/main/java/com/dchprojects/mydictionaryrestapi/data/my_dictionary_database.sql
+++ src/main/java/com/dchprojects/mydictionaryrestapi/data/my_dictionary_database.sql
@@ -13,14 +13,14 @@ CREATE TABLE `user` (
                         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                         updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                         PRIMARY KEY (user_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE `language` (
                             language_id INT NOT NULL AUTO_INCREMENT,
                             language_name TEXT NOT NULL,
                             created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                             PRIMARY KEY (language_id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE `course` (
                           user_id INT NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE `course` (
                           created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                           updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                           PRIMARY KEY (course_id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE `word` (
                         user_id INT NOT NULL,
@@ -42,4 +42,4 @@ CREATE TABLE `word` (
                         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                         updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                         PRIMARY KEY (word_id)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+);
