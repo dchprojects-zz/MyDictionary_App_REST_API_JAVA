82daad6dc0f864f30eaa46008fe041e766d88223
Dima <dima.chumakovwork@gmail.com>
2021-07-25 18:04:07 +0300

    deleted enable configuration

--- src/main/java/com/dchprojects/mydictionaryrestapi/MydictionaryRestApiApplication.java
+++ src/main/java/com/dchprojects/mydictionaryrestapi/MydictionaryRestApiApplication.java
@@ -1,11 +1,9 @@
 package com.dchprojects.mydictionaryrestapi;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class MydictionaryRestApiApplication {
 
 	public static void main(String[] args) {
