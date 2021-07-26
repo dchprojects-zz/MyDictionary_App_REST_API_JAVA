ea3b7693a07367870229dd5de498df92323489b9
Dima <dima.chumakovwork@gmail.com>
2021-07-26 21:59:42 +0300

    added swagger config file

---
+++ src/main/java/com/dchprojects/mydictionaryrestapi/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.dchprojects.mydictionaryrestapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.dchprojects.mydictionaryrestapi"))
+                .paths(regex("/api.*"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("My").build();
+    }
+
+}
