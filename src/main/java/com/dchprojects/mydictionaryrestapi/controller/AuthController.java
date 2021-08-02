06156e66961eb7c0ce43e7444abee8a31a1995cd
Dima <dima.chumakovwork@gmail.com>
2021-08-02 09:10:06 +0300

    added dependencies, jwtsecret, jwtExpirationInMs, authcontroller

---
+++ src/main/java/com/dchprojects/mydictionaryrestapi/controller/AuthController.java
@@ -0,0 +1,11 @@
+package com.dchprojects.mydictionaryrestapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+
+}
