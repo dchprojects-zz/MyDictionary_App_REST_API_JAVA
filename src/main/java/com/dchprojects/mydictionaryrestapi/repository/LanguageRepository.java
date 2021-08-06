9b1a5c9035c84ff95da991b8dbc3e69a99d8869f
Dima <dima.chumakovwork@gmail.com>
2021-08-06 15:11:22 +0300

    edited LanguageRepository file

--- src/main/java/com/dchprojects/mydictionaryrestapi/repository/LanguageRepository.java
+++ src/main/java/com/dchprojects/mydictionaryrestapi/repository/LanguageRepository.java
@@ -5,17 +5,11 @@ import com.dchprojects.mydictionaryrestapi.domain.entity.LanguageEntity;
 
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface LanguageRepository extends JpaRepository<LanguageEntity, Long> {
-
-    Optional<LanguageEntity> findByLanguageName(String languageName);
 
     Boolean existsByLanguageId(Long languageId);
 
     Boolean existsByLanguageName(String languageName);
-
-    Boolean existsByLanguageIdAndLanguageName(Long languageId, String languageName);
 
 }
