e43e20d21fe8510ff77c7778d7aebe82055b1708
Dima <dima.chumakovwork@gmail.com>
2021-10-27 15:43:51 +0300

    updated tomcat config for prevent the problem from happening every 15 minutes and speedup boot a bit.

--- src/main/java/com/dchprojects/mydictionaryrestapi/configuration/TomcatConfig.java
+++ src/main/java/com/dchprojects/mydictionaryrestapi/configuration/TomcatConfig.java
@@ -2,6 +2,7 @@ package com.dchprojects.mydictionaryrestapi.configuration;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.webresources.ExtractingRoot;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -11,14 +12,30 @@ import org.springframework.context.annotation.Configuration;
 public class TomcatConfig {
 
     @Bean
-    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainerCustomizer() {
-        return (TomcatServletWebServerFactory container) -> {
-            container.addContextCustomizers((Context context) -> {
-                // This configuration is used to improve initialization performance.
+    TomcatServletWebServerFactory tomcatFactory() {
+        return new TomcatServletWebServerFactory() {
 
+            @Override
+            protected void postProcessContext(Context context) {
                 context.setResources(new ExtractingRoot());
-                context.setReloadable(false);
-            });
+            }
+        };
+    }
+
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> servletContainerCustomizer() {
+        return new WebServerFactoryCustomizer<TomcatServletWebServerFactory>() {
+
+            @Override
+            public void customize(TomcatServletWebServerFactory container) {
+                container.addContextCustomizers(
+                        new TomcatContextCustomizer() {
+                            @Override
+                            public void customize(Context cntxt) {
+                                cntxt.setReloadable(false);
+                            }
+                        });
+            }
         };
     }
 
